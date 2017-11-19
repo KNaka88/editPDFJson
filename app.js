a1e7c596fa6322953c279e6b61112dddad646295
Koji Nakagawa <lightupthesky0627@gmail.com>
2017-11-18 18:50:17 -0800

    create lambda function

---
+++ app.js
@@ -0,0 +1,45 @@
+const path = require('path'),
+  os = require('os'),
+  ApiBuilder = require('claudia-api-builder'),
+  fs = require('./fs-promise'),
+  childProcess = require('./child-process-promise'),
+  api = new ApiBuilder();
+
+module.exports = api;
+
+
+api.post('/generate-pdf', (request) => {
+  'use strict';
+  const HummusRecipe = require('hummus-recipe'),
+        initialTempFile = `/tmp/${request.lambdaContext.awsRequestId}.pdf`,
+        finishedTempFile = `/tmp/${(Date.now()).toString()}.pdf`,
+        data = new Buffer(request.body, 'base64');
+  let result;
+
+
+  // create initialTempFile by reading buffer sent from POST method
+  return fs.writeFilePromise(initialTempFile, data)
+    .then( () => {
+
+      // edit initialTempFile and save changed data to finishedTempFile
+      const pdfDoc = new HummusRecipe(initialTempFile, finishedTempFile);
+
+      return pdfDoc
+        // edit detail
+        .editPage(1)
+        .text('hello world', 10, 10)
+        .endPage()
+        .endPDF();
+    })
+    .then( () => fs.readFilePromise(finishedTempFile))  //read buffer from finishedTempFile
+    .then( (fileContent) => result = fileContent)  // pass data to variable result
+    .then( () => fs.unlinkPromise(finishedTempFile)) // delete temp file
+    .then( () => fs.unlinkPromise(initialTempFile)) // delete temp file
+    .then( () => new Buffer(result).toString('base64')); // convert to base64 and return the data
+}, {
+  // if success, convert to binary data and send back as pdf file
+  success: {
+    contentType: 'application/pdf',
+    contentHandling: 'CONVERT_TO_BINARY'
+  }
+});
